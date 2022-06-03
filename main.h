e054454ce24ea57314525766aac8e9dbf6d8a04c
leul99teklu <leul99teklu@gmail.com>
2022-06-03 11:53:31 +0300

    [written] main.h of the project.

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
